24c070e7f0b1a0776f7ed18580bc6faf9954773a
Renee <renee@uleth.ca>
2023-12-24 22:43:48 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame
+import sys
+import random
+
+# Initialize Pygame
+pygame.init()
+
+# Contants
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+SCREEN_TITLE = 'Color Catcher'
+WHITE_COLOR = (255, 255, 255)
+FPS = 60
+
+# Player
+player_color = (255, 0, 0) # Red is the starting color
+
+
+# Game loop
